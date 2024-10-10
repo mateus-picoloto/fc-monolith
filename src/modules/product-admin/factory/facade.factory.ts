04cc80f63dc948dc1954bc41847eec82602f4b71
Mateus Picoloto <mateus.picoloto97@gmail.com>
2024-10-10 19:10:27 -0300

    feat: added product-adm-facade

---
+++ src/modules/product-admin/factory/facade.factory.ts
@@ -0,0 +1,14 @@
+import ProductAdmFacade from "../facade/product-adm.facade";
+import ProductRepository from "../repository/product.repository";
+import AddProductUseCase from "../usecase/add-product/add-product.usecase";
+
+export default class ProductAdmFacadeFactory {
+    static create(): ProductAdmFacade {
+        const productRepository = new ProductRepository();
+        const addProductUseCase = new AddProductUseCase(productRepository);
+        return new ProductAdmFacade({
+            addUseCase: addProductUseCase,
+            stockUseCase: undefined,
+        });
+    }
+}
