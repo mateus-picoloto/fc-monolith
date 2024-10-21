cddf5555f3607eeeea4fa7eadd4e173c4c68e5c1
Mateus Picoloto <mateus.picoloto97@gmail.com>
2024-10-21 20:32:44 -0300

    feat: added find and add client use cases

---
+++ src/modules/client-adm/usecase/add-client/add-client.usecase.spec.ts
@@ -0,0 +1,30 @@
+import AddClientUseCase from "./add-client.usecase";
+
+const MockRepository = () => {
+    return {
+        add: jest.fn(),
+        find: jest.fn(),
+    }
+};
+
+describe("Add client UseCase unit test", () => {
+    it("should add a client", async () => {
+        const repository = MockRepository();
+        const useCase = new AddClientUseCase(repository);
+
+        const input = {
+            id: "1",
+            name: "Client 1",
+            email: "x@x.com",
+            address: "Address 1",
+        };
+
+        const result = await useCase.execute(input);
+
+        expect(repository.add).toHaveBeenCalled();
+        expect(result.id).toEqual(input.id);
+        expect(result.name).toEqual(input.name);
+        expect(result.email).toEqual(input.email);
+        expect(result.address).toEqual(input.address);
+    });
+})
